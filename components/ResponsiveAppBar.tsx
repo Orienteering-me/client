6eebd7f29827ec8b7ac2fbd01f0c775c53a30c26
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2024-05-09 17:27:26 +0100

    Solved appbar bug

--- components/ResponsiveAppBar.tsx
+++ components/ResponsiveAppBar.tsx
@@ -1,74 +1,80 @@
-import * as React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Menu from "@mui/material/Menu";
-import Container from "@mui/material/Container";
-import Avatar from "@mui/material/Avatar";
-import MenuItem from "@mui/material/MenuItem";
+import { MouseEvent, useEffect, useState } from "react";
+import {
+  AppBar,
+  Avatar,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Menu,
+  MenuItem,
+  Toolbar,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-const pages = [
-  { text: "Iniciar sesión", route: "login" },
-  { text: "Registrarme", route: "register" },
+const pages = [{ text: "Información", href: "info" }];
+
+const menuItems = [
+  { text: "Iniciar sesión", href: "login" },
+  { text: "Registrarme", href: "register" },
 ];
 
-const authenticatedPages = [
-  { text: "Mi cuenta", route: "account" },
-  { text: "Carreras favoritas", route: "saved_courses" },
-  { text: "Crear carrera", route: "new_course" },
-  { text: "Carreras creadas", route: "created_courses" },
-  { text: "Cerrar sesión", route: "logout" },
+const authenticatedMenuItems = [
+  { text: "Mi cuenta", href: "account" },
+  { text: "Carreras favoritas", href: "saved_courses" },
+  { text: "Crear carrera", href: "new_course" },
+  { text: "Carreras creadas", href: "created_courses" },
+  { text: "Cerrar sesión", href: "logout" },
 ];
 
 function ResponsiveAppBar() {
-  const [authenticated, setAuthenticated] = React.useState(false);
+  const [authenticated, setAuthenticated] = useState(false);
 
-  React.useEffect(() => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
+
+  useEffect(() => {
     const token = localStorage.getItem("jwt-token");
     setAuthenticated(token != null);
   }, []);
 
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+  const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
   };
-
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
     setAnchorElUser(event.currentTarget);
   };
 
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
   };
 
   return (
     <AppBar position="static">
-      <Container sx={{ width: "100%", m: 0 }}>
+      <Container maxWidth={false}>
         <Toolbar disableGutters>
-          <Box component="a" href="." sx={{ display: "flex", mr: 1 }}>
+          <Box
+            component="a"
+            href="."
+            sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
+          >
             <Box
               component="img"
               sx={{
-                height: { xs: 48, md: 64 },
+                height: 80,
+                pb: 1,
               }}
               alt="Orienteering.me"
               src={"logo-icon.svg"}
             />
           </Box>
           <Typography
-            variant="h6"
+            variant="h4"
             noWrap
             component="a"
             href="."
@@ -77,43 +83,125 @@ function ResponsiveAppBar() {
               display: { xs: "none", md: "flex" },
               fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: ".3rem",
+              letterSpacing: ".1rem",
               color: "white",
               textDecoration: "none",
             }}
           >
             ORIENTEERING.ME
           </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }} />
 
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="pages"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="secondary"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.text}
+                  onClick={handleCloseNavMenu}
+                  component="a"
+                  href={page.href}
+                >
+                  <Typography
+                    textAlign="center"
+                    sx={{
+                      fontWeight: 500,
+                    }}
+                  >
+                    {page.text}
+                  </Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            component="a"
+            href="."
+            sx={{ display: { xs: "flex", md: "none" } }}
+          >
+            <Box
+              component="img"
+              sx={{
+                height: 48,
+                pb: 0.5,
+              }}
+              alt="Orienteering.me"
+              src={"logo-icon.svg"}
+            />
+          </Box>
           <Typography
             noWrap
             component="a"
-            href="#app-bar-with-responsive-menu"
+            href="."
             sx={{
               mr: 2,
               display: { xs: "flex", md: "none" },
               flexGrow: 1,
               fontFamily: "monospace",
               fontWeight: 700,
-              letterSpacing: ".3rem",
+              letterSpacing: ".1rem",
               color: "white",
               textDecoration: "none",
             }}
           >
             ORIENTEERING.ME
           </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}></Box>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page.text}
+                onClick={handleCloseNavMenu}
+                sx={{
+                  mr: 2,
+                  my: 2,
+                  color: "white",
+                  display: "block",
+                  fontSize: 20,
+                }}
+                href={page.href}
+              >
+                {page.text}
+              </Button>
+            ))}
+          </Box>
 
           <Box sx={{ flexGrow: 0 }}>
-            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-              <Avatar alt="Account" src="default_avatar.png" />
-            </IconButton>
+            <Tooltip title="Abrir menú">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
+                <Avatar alt="Avatar" src="default_avatar.png" />
+              </IconButton>
+            </Tooltip>
             <Menu
-              sx={{ mt: "45px" }}
               id="menu-appbar"
               anchorEl={anchorElUser}
               anchorOrigin={{
-                vertical: "top",
+                vertical: "bottom",
                 horizontal: "right",
               }}
               keepMounted
@@ -125,24 +213,38 @@ function ResponsiveAppBar() {
               onClose={handleCloseUserMenu}
             >
               {authenticated
-                ? authenticatedPages.map((page) => (
+                ? authenticatedMenuItems.map((item) => (
                     <MenuItem
-                      key={page.text}
+                      key={item.text}
                       onClick={handleCloseUserMenu}
                       component="a"
-                      href={page.route}
+                      href={item.href}
                     >
-                      <Typography textAlign="center">{page.text}</Typography>
+                      <Typography
+                        textAlign="center"
+                        sx={{
+                          fontWeight: 500,
+                        }}
+                      >
+                        {item.text}
+                      </Typography>
                     </MenuItem>
                   ))
-                : pages.map((page) => (
+                : menuItems.map((item) => (
                     <MenuItem
-                      key={page.text}
+                      key={item.text}
                       onClick={handleCloseUserMenu}
                       component="a"
-                      href={page.route}
+                      href={item.href}
                     >
-                      <Typography textAlign="center">{page.text}</Typography>
+                      <Typography
+                        textAlign="center"
+                        sx={{
+                          fontWeight: 500,
+                        }}
+                      >
+                        {item.text}
+                      </Typography>
                     </MenuItem>
                   ))}
             </Menu>
@@ -152,5 +254,4 @@ function ResponsiveAppBar() {
     </AppBar>
   );
 }
-
 export default ResponsiveAppBar;

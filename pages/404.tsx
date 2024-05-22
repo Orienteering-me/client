3bc84cb829245bc6f65dc5f2056d5f8ad7b9944c
Daniel Jorge Acosta <alu0101239187@ull.edu.es>
2024-05-22 12:01:13 +0100

    Added page 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,1 +1,12 @@
-// TODO
+import ForbiddenPage from "../components/ForbiddenPage";
+
+export default function NotFound() {
+  return (
+    <ForbiddenPage
+      title="Error 404"
+      message="La página a la que intentas acceder no existe"
+      button_href="/"
+      button_text="Volver al inicio"
+    />
+  );
+}
